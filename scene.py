77f34fd53d965de079b0285116044a16bdfcabe9
UBC Student <brettm01@students.cs.ubc.ca>
2023-12-28 11:12:19 -0800

    added to InfoTheory animation

--- scene.py
+++ scene.py
@@ -1,4 +1,6 @@
 from manim import *
+import numpy as np
+import math
 
 class PartOne(Scene):
     def construct(self):
@@ -42,35 +44,59 @@ class InfoTheory(Scene):
     def construct(self):
         #create elements
         LR = Square(color=GREEN, fill_opacity=0.5)
-        LR.shift(LEFT*2)
-        LR_label = Text("LR").scale(0.7)
+        LR.shift(LEFT*3)
+        LR_label = Text("Low Res").scale(0.6)
         LR_label.move_to(LR)
 
-        HR = Square(color=BLUE, fill_opacity=0.5)
-        HR.shift(RIGHT*2)
-        HR_label = Text("HR").scale(0.7)
-        HR_label.move_to(HR)
+        F_mapping = Circle(color=RED, fill_opacity=0.5).scale(0.5)
+        F_mapping_label = Text("F", color=WHITE, slant=ITALIC).scale(0.6)
 
-        NN = Circle(color=GRAY, fill_opacity=0.5)
+        HR_lower = Square(color=BLUE, fill_opacity=0.5)
+        HR_lower.shift(RIGHT*3)
+        HR_lower_label = Text("High Res").scale(0.6)
+        HR_lower_label.move_to(HR_lower)
+
+        HR_upper = Square(color=BLUE, fill_opacity=0.5)
+        HR_upper.shift(RIGHT*3)
+        HR_upper.shift(UP*1.5)
+        HR_upper_label = Text("High Res \n + Info").scale(0.6)
+        HR_upper_label.move_to(HR_upper)
+
+        NN = Circle(color=GRAY, fill_opacity=0.5).scale(0.5)
         NN.shift(UP*2)
-        NN_label = Text("NN").scale(0.7)
+        NN_label = Text("NN").scale(0.8)
         NN_label.move_to(NN)
 
-        arrow_across = Arrow(LR.get_center(), HR.get_center(), buff=1.2, color=GRAY)
-        arrow_down = Arrow(UP, DOWN, color=GRAY)
+        line_across_L = Line(LR.get_edge_center(RIGHT), F_mapping.get_edge_center(LEFT), color=WHITE)
+        line_across_R_down = Line(F_mapping.get_edge_center(RIGHT), HR_lower.get_edge_center(LEFT), color=WHITE)
+        line_across_R_up = Line(F_mapping.get_edge_center(RIGHT), HR_upper.get_edge_center(LEFT), color=WHITE)
+
+
+        # Angle in radians corresponding to 45 degrees
+        start_point = NN.get_center() + RIGHT*0.35 + DOWN*0.35
+        arrow = Line(start_point, line_across_R_up.get_center())
+        #arrow_down = Arrow(NN.get_center(), DOWN, buff=0.8, color=WHITE)
 
         #horizontal group for standard process
-        standard_group = VGroup(LR, LR_label, arrow_across, HR, HR_label)
+        #standard_group = VGroup(LR, LR_label, line_across_L, F_mapping, line_across_R, HR_lower, HR_lower_label)
 
         #animation
+        self.play(FadeIn(F_mapping), Write(F_mapping_label))
         self.play(FadeIn(LR), Write(LR_label))
-        self.play(Create(arrow_across))
-        self.play(FadeIn(HR), Write(HR_label))
-        self.play(standard_group.animate.shift(DOWN), FadeIn(NN), Write(NN_label))
-        self.play(Create(arrow_down))
+        self.play(Create(line_across_L))
+        self.wait()
+        self.play(Create(line_across_R_down))
+        self.play(FadeIn(HR_lower), Write(HR_lower_label))
+        self.play(HR_lower.animate.shift(DOWN*1.5), HR_lower_label.animate.shift(DOWN*1.5), 
+                  line_across_R_down.animate.put_start_and_end_on(F_mapping.get_edge_center(RIGHT), 
+                  HR_lower.get_edge_center(LEFT) + DOWN * 1.5))
+        self.play(FadeIn(NN), Write(NN_label))
+        self.play(Create(line_across_R_up), Create(arrow))
+        self.play(FadeIn(HR_upper), Write(HR_upper_label))
+        #self.play(Create(line_across_L))
+        #self.play(Write(line_label))
+        #self.play(Create(line_across_R))
+        #self.play(standard_group.animate.shift(DOWN), FadeIn(NN), Write(NN_label))
+        #self.play(Create(arrow_down))
         
-
-
-
-
 
