b726189af92e9ec492d3c666549e7937d9b23393
UBC Student <brettm01@students.cs.ubc.ca>
2023-12-28 20:47:20 -0800

    worked on quality metrics animation

--- scene.py
+++ scene.py
@@ -2,32 +2,24 @@ from manim import *
 import numpy as np
 import math
 
-class PartOne(Scene):
-    def construct(self):
-        degredation = MathTex(r"I_x = D(I_y; \delta)", font_size=64)
-        regeneration = MathTex(r"\hat{I_y} = F(I_x; \theta)", font_size=64)
-        part_one = VGroup(degredation, regeneration).arrange(DOWN)
-
-        self.add(part_one)
-
-class PartTwo(Scene):
-    def construct(self):
-        mse = MathTex(r"MSE = \frac{1}{mn}\sum_{i=0}^{m-1}\sum_{j=0}^{n-1}[X(i,j) - Y(i,j)]^2", font_size=64)
-        psnr = MathTex(r"10log_{10}\left(\frac{{MAX_I}^2}{MSE}\right)", font_size=64)
-
-        self.add(mse)
+#global elemnts
+Willy_standard = ImageMobject("assets/Willy-Standard.png").scale(0.1)
+Willy_pixelated = ImageMobject("assets/Willy-Pixelated.png").scale(0.1)
 
 class SSIM(Scene):
     def construct(self):
         luminance = MathTex(r"Luminance = \mu_x = \frac{1}{N}\sum_{i=1}^Nx_i")
         contrast = MathTex(r"Contrast = \sigma_x = \sqrt{\frac{1}{N-1}\sum_{i=1}^{N}(x_i - \mu_x)^2}", font_size=64)
         sigma_xy = MathTex(r"\sigma_{xy} = \frac{1}{N-1}\sum_{i=1}^{N}(x_i - \mu_i)(y_i - \mu_y)", font_size=64)
+        
         l_comp = MathTex(r"l(x,y) = \frac{2\mu_x\mu_y + C_1}{{\mu_x}^2 + {\mu_y}^2 + C_1}", font_size=64)
         c_comp = MathTex(r"c(x,y) = \frac{2\sigma_x\sigma_y + C_2}{{\sigma_x}^2 + {\sigma_y}^2 + C_2}", font_size=64)
         s_comp = MathTex(r"s(x,y) = \frac{\sigma_{xy} + C_3}{\sigma_x\sigma_y + C_3}", font_size=64)
+       
         ssim = MathTex(r"SSIM(x,y) = [l(x,y)]^{\alpha}[c(x,y)]^{\beta}[s(x,y)]^{\gamma}", font_size=64)
         ssim_cond = MathTex(r"where\ (\alpha, \beta, \gamma) > 0")
         ssim_group = VGroup(ssim, ssim_cond).arrange(DOWN)
+       
         mssim = MathTex(r"MSSIM(X,Y) = \frac{1}{M}\sum_{j=1}^{M}SSIM(x_j, y_j)", font_size=64)
 
         self.add(mssim)
@@ -43,7 +35,7 @@ class PartThree(Scene):
 class InfoTheory(Scene):
     def construct(self):
         #create elements
-        LR = Square(color=GREEN, fill_opacity=0.5)
+        LR = Square(color=GRAY, fill_opacity=0.5)
         LR.shift(LEFT*3)
         LR_label = Text("Low Res").scale(0.6)
         LR_label.move_to(LR)
@@ -51,52 +43,103 @@ class InfoTheory(Scene):
         F_mapping = Circle(color=RED, fill_opacity=0.5).scale(0.5)
         F_mapping_label = Text("F", color=WHITE, slant=ITALIC).scale(0.6)
 
-        HR_lower = Square(color=BLUE, fill_opacity=0.5)
-        HR_lower.shift(RIGHT*3)
-        HR_lower_label = Text("High Res").scale(0.6)
-        HR_lower_label.move_to(HR_lower)
+        HR = Square(color=BLUE, fill_opacity=0.5)
+        HR.shift(RIGHT*3)
+        HR_label = Text("High Res").scale(0.6)
+        HR_label.move_to(HR)
 
         HR_upper = Square(color=BLUE, fill_opacity=0.5)
         HR_upper.shift(RIGHT*3)
         HR_upper.shift(UP*1.5)
-        HR_upper_label = Text("High Res \n + Info").scale(0.6)
+        HR_upper_label = Text("High Res").scale(0.6)
         HR_upper_label.move_to(HR_upper)
 
-        NN = Circle(color=GRAY, fill_opacity=0.5).scale(0.5)
+        NN = Circle(color=RED, fill_opacity=0.5).scale(0.5)
         NN.shift(UP*2)
         NN_label = Text("NN").scale(0.8)
         NN_label.move_to(NN)
 
         line_across_L = Line(LR.get_edge_center(RIGHT), F_mapping.get_edge_center(LEFT), color=WHITE)
-        line_across_R_down = Line(F_mapping.get_edge_center(RIGHT), HR_lower.get_edge_center(LEFT), color=WHITE)
+        line_across_R_down = Line(F_mapping.get_edge_center(RIGHT), HR.get_edge_center(LEFT), color=WHITE)
         line_across_R_up = Line(F_mapping.get_edge_center(RIGHT), HR_upper.get_edge_center(LEFT), color=WHITE)
-
 
         # Angle in radians corresponding to 45 degrees
         start_point = NN.get_center() + RIGHT*0.35 + DOWN*0.35
         arrow = Line(start_point, line_across_R_up.get_center())
-        #arrow_down = Arrow(NN.get_center(), DOWN, buff=0.8, color=WHITE)
 
-        #horizontal group for standard process
-        #standard_group = VGroup(LR, LR_label, line_across_L, F_mapping, line_across_R, HR_lower, HR_lower_label)
-
-        #animation
+        #animate
         self.play(FadeIn(F_mapping), Write(F_mapping_label))
         self.play(FadeIn(LR), Write(LR_label))
         self.play(Create(line_across_L))
         self.wait()
         self.play(Create(line_across_R_down))
-        self.play(FadeIn(HR_lower), Write(HR_lower_label))
-        self.play(HR_lower.animate.shift(DOWN*1.5), HR_lower_label.animate.shift(DOWN*1.5), 
+        self.play(FadeIn(HR), Write(HR_label))
+        self.play(HR.animate.shift(DOWN*1.5), HR_label.animate.shift(DOWN*1.5), 
                   line_across_R_down.animate.put_start_and_end_on(F_mapping.get_edge_center(RIGHT), 
-                  HR_lower.get_edge_center(LEFT) + DOWN * 1.5))
+                  HR.get_edge_center(LEFT) + DOWN * 1.5))
         self.play(FadeIn(NN), Write(NN_label))
         self.play(Create(line_across_R_up), Create(arrow))
         self.play(FadeIn(HR_upper), Write(HR_upper_label))
-        #self.play(Create(line_across_L))
-        #self.play(Write(line_label))
-        #self.play(Create(line_across_R))
-        #self.play(standard_group.animate.shift(DOWN), FadeIn(NN), Write(NN_label))
-        #self.play(Create(arrow_down))
-        
 
+class FormalDef(Scene):
+    def construct(self):
+        #create elements
+        degredation = MathTex(r"I_x = D(I_y; \delta)", font_size=64)
+        regeneration = MathTex(r"\hat{I_y} = F(I_x; \theta)", font_size=64)
+
+        #animate
+        self.play(Write(degredation))
+        self.wait(2)
+        self.play(Transform(degredation, regeneration))
+
+class QualityMetrics(Scene):
+    def construct(self):
+        #create elements
+        mse = MathTex(r"MSE = \frac{1}{mn}\sum_{i=0}^{m-1}\sum_{j=0}^{n-1}[X(i,j) - Y(i,j)]^2", font_size=64)
+        psnr = MathTex(r"PSNR = 10log_{10}\left(\frac{{MAX_I}^2}{MSE}\right)", font_size=64)
+
+        Loc_Willy_standard = Willy_standard
+        #Loc_Willy_standard.shift(LEFT)
+        Loc_Willy_pixelated = Willy_pixelated
+        Loc_Willy_pixelated.shift(RIGHT*4)
+
+        GT_label = Text("Ground Truth").scale(0.6)
+        GT_label.next_to(Loc_Willy_standard, DOWN)
+        OP_label = Text("HR Output").scale(0.6)
+        OP_label.next_to(Loc_Willy_pixelated, DOWN)
+
+        left_bracket = Brace(Loc_Willy_standard, direction=LEFT)
+        left_bracket.next_to(Loc_Willy_standard, LEFT)
+        right_bracket = Brace(Loc_Willy_pixelated, direction=RIGHT)
+        right_bracket.next_to(Loc_Willy_pixelated, RIGHT)
+
+        subtraction_symbol = MathTex("-", font_size=64)
+        subtraction_symbol.next_to(Loc_Willy_standard, RIGHT)
+        subtraction_symbol.next_to(Loc_Willy_pixelated, LEFT)
+
+        squared_symbol = MathTex(r"2", font_size=64)
+        squared_symbol.next_to(right_bracket, UP, buff=-0.2)
+        squared_symbol.shift(RIGHT*0.3)
+        
+        sum_symbol = MathTex(r"\sum", font_size=96)
+        sum_symbol.next_to(left_bracket, LEFT)
+        
+        mse_label = MathTex(r"MSE = ", font_size=64)
+        mse_label.next_to(sum_symbol, LEFT)
+
+        #animate
+        self.play(Write(mse))
+        self.wait(2)
+        self.play(Transform(mse, mse_label))
+        self.play(Write(sum_symbol), Create(left_bracket), 
+                  Write(GT_label), Write(subtraction_symbol), Write(OP_label), 
+                  Create(right_bracket), Write(squared_symbol))
+        self.play(FadeIn(Loc_Willy_standard), FadeIn(Loc_Willy_pixelated))
+
+        #animation of mse for image evalion
+        #self.play(Create(left_bracket), Write(subtraction_symbol), Create(right_bracket))
+
+
+        #self.play(Transform(mse, psnr))
+        #self.wait(2)
+        
