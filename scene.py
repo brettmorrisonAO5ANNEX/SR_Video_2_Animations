8a4cf60665b42b8f6449ab7d301146ec7ccec18f
UBC Student <brettm01@students.cs.ubc.ca>
2023-12-31 11:25:02 -0800

    began updating animation + redesigning for consistency

--- scene.py
+++ scene.py
@@ -21,7 +21,6 @@ class Intro(Scene):
 
 class InfoTheory(Scene):
     def construct(self):
-        #create elements
         LR = Square(color=GRAY, fill_opacity=0.5)
         LR.shift(LEFT*3)
         LR_label = Text("Low Res").scale(0.6)
@@ -30,15 +29,15 @@ class InfoTheory(Scene):
         F_mapping = Circle(color=RED, fill_opacity=0.5).scale(0.5)
         F_mapping_label = Text("F", color=WHITE, slant=ITALIC).scale(0.6)
 
-        HR = Square(color=BLUE, fill_opacity=0.5)
+        HR = Square(color=GREEN, fill_opacity=0.5)
         HR.shift(RIGHT*3)
-        HR_label = Text("High Res").scale(0.6)
+        HR_label = Text("High Res 1").scale(0.6)
         HR_label.move_to(HR)
 
         HR_upper = Square(color=BLUE, fill_opacity=0.5)
         HR_upper.shift(RIGHT*3)
         HR_upper.shift(UP*1.5)
-        HR_upper_label = Text("High Res").scale(0.6)
+        HR_upper_label = Text("High Res 2").scale(0.6)
         HR_upper_label.move_to(HR_upper)
 
         NN = Circle(color=RED, fill_opacity=0.5).scale(0.5)
@@ -54,7 +53,6 @@ class InfoTheory(Scene):
         start_point = NN.get_center() + RIGHT*0.35 + DOWN*0.35
         arrow = Line(start_point, line_across_R_up.get_center())
 
-        #animate
         self.play(FadeIn(F_mapping), Write(F_mapping_label))
         self.play(FadeIn(LR), Write(LR_label))
         self.play(Create(line_across_L))
@@ -70,14 +68,87 @@ class InfoTheory(Scene):
 
 class FormalDef(Scene):
     def construct(self):
-        #create elements
-        degredation = MathTex(r"I_x = D(I_y; \delta)", font_size=64)
-        regeneration = MathTex(r"\hat{I_y} = F(I_x; \theta)", font_size=64)
+        degredation_group = VGroup()
 
-        #animate
+        OG = Square(color=YELLOW, fill_opacity=0.5)
+        OG.shift(LEFT*3)
+        OG_label = MathTex(r"I_y", font_size=64)
+        OG_label.move_to(OG)
+        GT_label = Text("Ground Truth").scale(0.6)
+        GT_label.next_to(OG, UP, buff=0.1)
+        degredation_group.add(OG, OG_label, GT_label)
+
+        D_mapping = Circle(color=ORANGE, fill_opacity=0.5).scale(0.5)
+        D_mapping_label = MathTex(r"D", font_size=64)
+        degredation_group.add(D_mapping, D_mapping_label)
+
+        D_mapping_params = Circle(color=WHITE, fill_opacity=0.5).scale(0.5)
+        D_mapping_params_label = MathTex(r"\delta", font_size=64)
+        D_mapping_params.shift(UP*2)
+        D_mapping_params_label.shift(UP*2)
+        degredation_group.add(D_mapping_params, D_mapping_params_label)
+
+        DG = Square(color=GRAY, fill_opacity=0.5)
+        DG.shift(RIGHT*3)
+        DG_label = MathTex(r"I_x", font_size=64)
+        DG_label.move_to(DG)
+        LowR_label = Text("Low Res").scale(0.6)
+        LowR_label.next_to(DG, UP, buff=0.1)
+
+        degredation = MathTex(r"I_x = D(I_y; \delta)", font_size=64)
+        degredation.shift(DOWN*2)
+        degredation_group.add(degredation)
+
+        self.play(FadeIn(OG, OG_label), FadeIn(D_mapping, D_mapping_label), Write(GT_label))
+        self.play(FadeIn(D_mapping_params, D_mapping_params_label))
+
+        param_connection = Arrow(D_mapping_params.get_edge_center(DOWN), D_mapping.get_edge_center(UP))
+        input_connection = Line(OG.get_edge_center(RIGHT), D_mapping.get_edge_center(LEFT))
+        output_connection = Line(D_mapping.get_edge_center(RIGHT), DG.get_edge_center(LEFT))
+        degredation_group.add(param_connection, input_connection, output_connection)
+
+        self.play(Create(param_connection))
+        self.play(Create(input_connection))
+        self.play(Create(output_connection))
+        self.play(FadeIn(DG, DG_label), Write(LowR_label))
         self.play(Write(degredation))
-        self.wait(2)
-        self.play(Transform(degredation, regeneration))
+        self.wait()
+        self.play(FadeOut(degredation_group))
+        self.wait()
+
+        HR_output = Square(color=BLUE, fill_opacity=0.5)
+        HR_output.shift(RIGHT*3)
+        HR_output_label = MathTex(r"\hat{I_y}", font_size=64)
+        HR_output_label.move_to(HR_output)
+        HR_top_label = Text("High Res Output").scale(0.6)
+        HR_top_label.next_to(HR_output, UP, buff=0.1)
+
+        F_mapping = Circle(color=ORANGE, fill_opacity=0.5).scale(0.5)
+        F_mapping_label = MathTex(r"F", font_size=64)
+
+        F_mapping_params = Circle(color=WHITE, fill_opacity=0.5).scale(0.5)
+        F_mapping_params_label = MathTex(r"\theta", font_size=64)
+        F_mapping_params.shift(UP*2)
+        F_mapping_params_label.shift(UP*2)
+
+        regeneration = MathTex(r"\hat{I_y} = F(I_x; \theta)", font_size=64)
+        regeneration.shift(DOWN*2)
+
+        self.play(DG.animate.shift(LEFT*6), DG_label.animate.shift(LEFT*6), LowR_label.animate.shift(LEFT*6),
+                  FadeIn(F_mapping, F_mapping_label))
+        self.play(FadeIn(F_mapping_params, F_mapping_params_label))
+
+        param_connection = Arrow(F_mapping_params.get_edge_center(DOWN), F_mapping.get_edge_center(UP))
+        input_connection = Line(DG.get_edge_center(RIGHT), F_mapping.get_edge_center(LEFT))
+        output_connection = Line(F_mapping.get_edge_center(RIGHT), HR_output.get_edge_center(LEFT))
+
+        self.play(Create(param_connection))
+        self.play(Create(input_connection))
+        self.play(Create(output_connection))
+        self.play(FadeIn(HR_output, HR_output_label), Write(HR_top_label))
+        self.play(Write(regeneration))
+        self.wait()
+
 
 class QualityMetrics(Scene):
     def construct(self):  
@@ -113,12 +184,12 @@ class QualityMetrics(Scene):
         mse_label = MathTex(r"MSE = ", font_size=64)
         mse_label.next_to(sum_symbol, LEFT)
 
-        GT_background = Square(color=GREEN, fill_opacity=0.5)
+        GT_background = Square(color=YELLOW, fill_opacity=0.5)
         GT_background.shift(RIGHT*2)
         GT_background_label = Text("GT").scale(0.6)
         GT_background_label.move_to(GT_background)
 
-        LR = Square(color=BLUE, fill_opacity=0.5)
+        LR = Square(color=GRAY, fill_opacity=0.5)
         LR.shift(LEFT*2)
         LR_label = Text("HR Output").scale(0.6)
         LR_label.move_to(LR)
